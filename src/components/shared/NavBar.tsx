f9e06c34d97b1448a98e212300e61f9633f01924
miguelvelezsk <miguelvelezaguirre10@gmail.com>
2026-04-22 12:49:19 -0500

    feat: implement search functionality and add UI components for product results

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -1,6 +1,6 @@
 export default function NavBar() {
     return (
-        <header className="flex h-18 shrink-0 items-center px-2 md:px-6 border-b-2 border-[#F1F2F8] bg-tertiary">
+        <nav className="flex h-18 shrink-0 items-center px-2 md:px-6 border-b-2 border-[#F1F2F8] bg-tertiary">
             <div className="w-full px-2 md:px-7 flex items-center justify-between mx-auto">
                 <a href="/home" className="flex items-center space-x-2 cursor-pointer">
                     <span className="text-xl font-bold font-header text-primary">Ganga</span>
@@ -9,6 +9,6 @@ export default function NavBar() {
                     <a href="/home" className="text-lg underline underline-offset-10 decoration-2 font-medium cursor-pointer font-body text-primary">Inicio</a>
                 </div>
             </div>
-        </header>
+        </nav>
     )
 }
---
+++ src/components/shared/SearchBar.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { Search, X } from 'lucide-react';
+
+interface SearchBarProps {
+    setProductSearch: (productSearch: string) => void;
+    productSearch: string;
+}
+
+export default function SearchBar({ setProductSearch, productSearch }: SearchBarProps) {
+    const [productName, setProductName] = useState('');
+
+    const handleSearch = () => {
+        if (productName.trim()) {
+            setProductSearch(productName.trim());
+        }
+        console.log("Product Search: ", productSearch);
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            handleSearch();
+            console.log("Product Search: ", productSearch);
+        }
+    };
+
+    const handleClear = () => {
+        setProductName('');
+        setProductSearch('');
+    };
+
+    return (
+        <header className="flex shrink-0 px-12 items-start w-full">
+            <div className="flex flex-col items-start justify-center w-full gap-3 mt-10 mb-5">
+                <h1 className="font-header text-primary font-semibold text-2xl md:text-4xl mb-5">Encuentra el mejor precio para tus productos</h1>
+                <div className="flex items-center h-16 w-full md:w-2/4 h-14 px-1 pr-4 text-sm md:text-lg border border-neutral rounded-[0.5rem] bg-tertiary overflow-hidden">
+                    <input placeholder="Buscar productos..." value={productName} className="w-full h-full px-4 outline-none bg-transparent placeholder:text-neutral" type="text" onChange={(e) => setProductName(e.target.value)} onKeyDown={handleKeyDown} />
+                    {productName.length > 0 && (
+                        <button
+                            onClick={handleClear}
+                            className="flex items-center justify-center w-5 h-5 text-neutral hover:text-primary"
+                        >
+                            <X size={24} />
+                        </button>
+                    )}
+                    <button
+                        onClick={handleSearch}
+                        className="flex items-center justify-center rounded-3xl w-15 h-10 hover:bg-hover font-inter bg-secondary cursor-pointer text-white ml-3 rounded-[0.5rem]"
+                    >
+                        <Search size={24} />
+                    </button>
+                </div>
+            </div>
+        </header>
+    )
+
+}
